426f16138b2ca01ba8f9c9bf0602b0cb57e84f3c
Harald Pretl <harald@pretl.eu>
2026-08-22 10:15:04 +0200

    Update Kactus2 build args and version

--- _build/images/kactus2/scripts/install.sh
+++ _build/images/kactus2/scripts/install.sh
@@ -6,12 +6,12 @@
 set -e
 cd /tmp || exit 1
 
-git clone --filter=blob:none "${KACTUS_REPO_URL}" "${KACTUS_NAME}"
-cd "${KACTUS_NAME}" || exit 1
-git checkout "${KACTUS_REPO_COMMIT}"
-sed -i "s|^LOCAL_INSTALL_DIR=\".*\"|LOCAL_INSTALL_DIR=\"${TOOLS}/${KACTUS_NAME}\"|" .qmake.conf
+git clone --filter=blob:none "${KACTUS2_REPO_URL}" "${KACTUS2_NAME}"
+cd "${KACTUS2_NAME}" || exit 1
+git checkout "${KACTUS2_REPO_COMMIT}"
+sed -i "s|^LOCAL_INSTALL_DIR=\".*\"|LOCAL_INSTALL_DIR=\"${TOOLS}/${KACTUS2_NAME}\"|" .qmake.conf
 ./configure
 make -j"$(nproc)"
 make install
 
-echo "${KACTUS_NAME} ${KACTUS_REPO_COMMIT}" > "${TOOLS}/${KACTUS_NAME}/SOURCES"
+echo "${KACTUS2_NAME} ${KACTUS2_REPO_COMMIT}" > "${TOOLS}/${KACTUS2_NAME}/SOURCES"
