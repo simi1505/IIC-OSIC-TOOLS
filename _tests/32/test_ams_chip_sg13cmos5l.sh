98fb177e4ca793764681516b67ebb3e9152c74ce
Simon Dorrer <simon.dorrer@jku.at>
2026-08-25 11:06:27 +0200

    updated `test_ams_chip_sg13cmos5l.sh`

--- _tests/32/test_ams_chip_sg13cmos5l.sh
+++ _tests/32/test_ams_chip_sg13cmos5l.sh
@@ -24,7 +24,7 @@ cd "$TMP" || exit 1
 
 # Clone the main branch of the AMS chip template (incl. submodules)
 [ "$DEBUG" = 1 ] && echo "[INFO] Cloning $REPO (main branch, incl. submodules) ..."
-if ! git clone --depth 1 --recursive --shallow-submodules --branch main \
+if ! git clone --depth 1 --recursive --shallow-submodules --branch next_release \
         https://github.com/iic-jku/"$REPO".git "$REPO" > "$LOG" 2>&1; then
     echo "[ERROR] Test <AMS chip template with ihp-sg13cmos5l> FAILED! Could not clone the repository. Check the log file $LOG for details."
     exit 1
